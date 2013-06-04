b71c99e7af5bab5234b5247d5a7a87b8eeb74498
U-DIRECON\lhernandez <lhernandez@LHERNAN-3-SM-D1.direcon.local>
2013-06-04 16:06:49 -0400

    ok

---
+++ web/commands/Resources.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once('GenericCommand.php');
+include_once('../classes/Repuesto.php');
+include_once('../classes/Plataforma.php');
+include_once('../classes/Region.php');
+include_once('../classes/Fabricante.php');
+include_once('../classes/Motivo.php');
+include_once('../classes/Usuario.php');
+
+class DisplayHome extends GenericCommand {
+	function execute(){
+		global $fc;
+
+		$this->processSuccess();
+	}
+}
+?>
